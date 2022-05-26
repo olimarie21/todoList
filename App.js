c44ad1800cb27dc311857084607723d5dc0335e0
olimarie21 <olivia.underdah@gmail.com>
2022-05-26 12:08:14 -0700

    Add state to add/remove tasks

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Keyboard, Platform, TextInput, TouchableOpacity } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    Keyboard.dismiss();
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1); // remove the one item from the array
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
       {/* {Today's Tasks} */}
@@ -11,11 +26,32 @@ export default function App() {
 
         <View style={styles.items}>
           {/* Tasks go here */}
-          <Task text={'Task 1'}/>
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item}/>
+              </TouchableOpacity>
+            )
+          })          
+          }
         </View>
       </View>
 
+      {/* Write a Task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper} 
+      >
+        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)}/>
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
     </View>
+
   );
 }
 
@@ -35,6 +71,33 @@ const styles = StyleSheet.create({
   }, 
   items: {
     marginTop: 30
-  }
-          
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: "#c0c0c0",
+    width: 250,
+  }, 
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: 60,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  addText: {
+
+  },      
 });
